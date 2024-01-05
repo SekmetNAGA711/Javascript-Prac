fcbd14f1bd0112db4cf2078c565882190065fe25
AbbyANG <bena.ao310@gmail.com>
2024-01-05 12:41:05 -0500

    updated

---
+++ index.js
@@ -0,0 +1,26 @@
+var message = "Hello";
+var myName = 'Angela';
+console.log("Hello " + myName);
+
+var twitterCount = "Nice to meet you Angela";
+var remainingCount = 182 - twitterCount.length;
+
+console.log("You have " +remainingCount + " words left")
+
+
+var tweet = "what is your tweet?: "
+
+
+
+var cutOfPoint = tweet.slice(0,10);
+
+
+
+console.log(cutOfPoint);
+
+
+var yourName = "ballon";
+
+var yourUpper = yourName.toUpperCase();
+
+console.log(yourUpper);
