9a22679757c6732a24590bccebc65eff3c5dd156
AbbyANG <bena.ao310@gmail.com>
2024-03-05 17:11:49 -0500

    updated

--- interviewprep.js
+++ interviewprep.js
@@ -8,3 +8,32 @@ function bark (name, age) {
 }
 
 bark("kim", 9)
+
+var genres = []; 
+genres.push("Rockabilly"); 
+genres.push("Ambient"); 
+var size = genres.length;
+
+console.log(size)
+
+
+var taxi = {
+     make: "Webville Motors",
+     model: "Taxi",
+     year: 1955,
+     color: "yellow",
+     passengers: 4, 
+     convertible: false, 
+     mileage: 281341
+    };
+
+    function prequal(car) {
+    if (car.mileage > 10000) {
+    return false;
+    } 
+    else if (car.year > 1960) {
+    return false; 
+       }
+    return true; 
+}
+     console.log(prequal(taxi));
