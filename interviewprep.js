a9b8a93d9b80f3315ec068fb519a198fc20192f3
AbbyANG <bena.ao310@gmail.com>
2024-03-05 14:32:19 -0500

    updated

---
+++ interviewprep.js
@@ -0,0 +1,10 @@
+function bark (name, age) {
+    if(age > 8){
+        console.log( name + " sorry you are not the right age")
+    }
+    else if (age < 8) {
+        console.log(name + " you are the perfect age")
+    }
+}
+
+bark("kim", 9)
